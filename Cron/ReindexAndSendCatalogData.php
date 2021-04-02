453c659bb2b85ff2aea383acb9d75339602c8820
Danilo Argentiero <argentiero.danilo@gmail.com>
2021-04-02 13:03:00 +0200

    improve: add crontab to handle the reindex and send to Coveo

---
+++ Cron/ReindexAndSendCatalogData.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Coveo\Search\Cron;
+
+use Coveo\Search\Api\Service\Indexer\DataSenderInterface;
+use Coveo\Search\Api\Service\Indexer\CatalogInterface;
+
+class ReindexAndSendCatalogData
+{
+    /**
+     * @param CatalogInterface $dataSender
+     */
+    private $catalog;
+
+    /**
+     * @param DataSenderInterface $dataSender
+     */
+    private $dataSender;
+
+    /**
+     * @param CatalogInterface $catalog
+     * @param DataSenderInterface $dataSender
+     */
+    public function __construct(
+        CatalogInterface $catalog,
+        DataSenderInterface $dataSender
+    ) {
+        $this->catalog = $catalog;
+        $this->dataSender = $dataSender;
+    }
+
+    public function execute()
+    {
+        $this->catalog->execute();
+        $this->dataSender->execute();
+    }
+}
